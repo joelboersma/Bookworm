8cf6356f1ab0f2e01c453a40f1e9fc7d6712be96
Urvashi Mahto <urvashi.mahto16@gmail.com>
2021-02-23 01:06:52 -0800

    display table in listings view. wip: spacing between add & filter buttons, list view branches

--- Bookworm/ListingsViewController.swift
+++ Bookworm/ListingsViewController.swift
@@ -3,17 +3,29 @@
 //  Bookworm
 //
 //  Created by Christina Luong on 2/22/21.
-//
+//  Update by Urvashi Mahto on 2/22/21.
 
 import UIKit
 import Firebase
 
-class ListingsViewController: UIViewController {
+class ListingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource  {
 
+    @IBOutlet weak var listingsTableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
+            assertionFailure("Cell dequeue error")
+            return UITableViewCell.init()
+        }
+        return cell
+    }
 }
