2df80a8f9288d21d6c5ddccb483d1df4efb805dc
Urvashi Mahto <urvashi.mahto16@gmail.com>
2021-02-26 10:26:21 -0800

    added placeholder listings in listingsVC and linked dblistingVC popup on selecting cell

--- Bookworm/ListingsViewController.swift
+++ Bookworm/ListingsViewController.swift
@@ -16,15 +16,17 @@ class ListingsViewController: UIViewController, UISearchBarDelegate, UITableView
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var filterButton: UIButton!
     
+    var placeholderTitles: [String] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        placeholderTitles.append("ECS 171 Textbook")
+        placeholderTitles.append("ECS 150 Textbook")
+        placeholderTitles.append("FMS 001 Textbook")
         self.searchBar.delegate = self
-        
-        // Create tap gesture object for dismissing keyboard.
-        let tapGesture = UITapGestureRecognizer(target: self.view, action: #selector(UIView.endEditing))
-        
-        // Add tap gesture to view.
-        view.addGestureRecognizer(tapGesture)
+        listingsTableView.dataSource = self
+        listingsTableView.delegate = self
+        listingsTableView.reloadData()
     }
     
     @IBAction func filterButtonClicked(_ sender: Any) {
@@ -41,17 +43,6 @@ class ListingsViewController: UIViewController, UISearchBarDelegate, UITableView
     
     @IBAction func addRequestButtonClicked(_ sender: Any) {
         //add request
-        
-        // dblistingVC for now
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let vc = storyboard.instantiateViewController(identifier: "dblistingVC")
-        guard let dblistingVC = vc as? DatabaseListingViewController else {
-            assertionFailure("couldn't find vc")
-            return
-        }
-        //dblistingVC.delegate = self
-        
-        present(dblistingVC, animated: true, completion: nil)
     }
     
     @IBAction func addListingButtonClicked(_ sender: Any) {
@@ -64,14 +55,28 @@ class ListingsViewController: UIViewController, UISearchBarDelegate, UITableView
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return placeholderTitles.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell") else {
-            assertionFailure("Cell dequeue error")
-            return UITableViewCell.init()
-        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listingCell") ?? UITableViewCell(style: .default, reuseIdentifier: "listingCell")
+        
+        assert(indexPath.section == 0)
+        cell.textLabel?.text = placeholderTitles[indexPath.row]
         return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+        // empty dblistingVC for now
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(identifier: "dblistingVC")
+        guard let dblistingVC = vc as? DatabaseListingViewController else {
+            assertionFailure("couldn't find vc")
+            return
+        }
+        //dblistingVC.delegate = self
+
+        present(dblistingVC, animated: true, completion: nil)
     }
 }
