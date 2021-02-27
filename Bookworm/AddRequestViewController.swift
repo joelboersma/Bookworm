323d9b2560e93852aebb08266131c0b0d11e91f8
Joel Boersma <44932998+joelboersma@users.noreply.github.com>
2021-02-26 17:52:03 -0800

    AddRequest UISearchBarDelegate setup

--- Bookworm/AddRequestViewController.swift
+++ Bookworm/AddRequestViewController.swift
@@ -7,29 +7,24 @@
 
 import UIKit
 
-class AddRequestViewController: UIViewController {
+class AddRequestViewController: UIViewController, UISearchBarDelegate {
 
     // Search API
     // https://openlibrary.org/dev/docs/api/search
     
+    @IBOutlet weak var searchBar: UISearchBar!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        searchBar.delegate = self
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        // Do search functions here
     }
     
     @IBAction func didPressBack(_ sender: Any) {
         self.navigationController?.popViewController(animated: true)
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
