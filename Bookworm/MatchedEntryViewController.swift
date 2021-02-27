11026b595d94812380db80c14268f01b3f21b83c
Mohammed Haque <msyamhaque@gmail.com>
2021-02-26 16:48:50 -0800

    created matched entry vc similar to db listing and added an alert when transaction button pressed.

---
+++ Bookworm/MatchedEntryViewController.swift
@@ -0,0 +1,48 @@
+//
+//  MatchedEntryViewController.swift
+//  Bookworm
+//
+//  Created by Mohammed Haque on 2/26/21.
+//
+
+import Foundation
+import UIKit
+import Firebase
+import MessageUI
+
+class MatchedEntryViewController: UIViewController, MFMessageComposeViewControllerDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after lsoading the view.
+    }
+    
+    @IBAction func contactButtonPressed() {
+        let controller = MFMessageComposeViewController()
+        controller.messageComposeDelegate = self
+        controller.body = "Hello from Book Worm!"
+        // using my phone number as placeholder
+        controller.recipients = ["+1 510 403 5014"]
+        if MFMessageComposeViewController.canSendText() {
+            self.present(controller, animated: true, completion: nil)
+        }
+    }
+    
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func xButtonPressed() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func transactionButtonPressed(_ sender: Any) {
+        let alert = UIAlertController(title: "Was <BOOK_TITLE> bought/sold from/to <USERNAME>?", message: nil, preferredStyle: .alert)
+
+        alert.addAction(UIAlertAction(title: "Yes, remove from wishlist/inventory", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "Yes, keep in wishlist/inventory", style: .default, handler: nil))
+        alert.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
+
+        self.present(alert, animated: true)
+    }
+}
--- Bookworm/MatchedViewController.swift
+++ Bookworm/MatchedViewController.swift
@@ -8,10 +8,13 @@
 import Foundation
 import CoreLocation
 import UIKit
+import Firebase
 
-class MatchedViewController: UIViewController, CLLocationManagerDelegate {
+class MatchedViewController: UIViewController, CLLocationManagerDelegate, UITableViewDelegate, UITableViewDataSource {
     
+    @IBOutlet var matchesTableView: UITableView!
     let locationManager = CLLocationManager()
+    var placeholderTitles: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,6 +33,13 @@ class MatchedViewController: UIViewController, CLLocationManagerDelegate {
             locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
             locationManager.startUpdatingLocation()
         }
+        
+        placeholderTitles.append("ECS 171 Textbook")
+        placeholderTitles.append("ECS 150 Textbook")
+        placeholderTitles.append("FMS 001 Textbook")
+        matchesTableView.dataSource = self
+        matchesTableView.delegate = self
+        matchesTableView.reloadData()
     }
 
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
@@ -37,5 +47,29 @@ class MatchedViewController: UIViewController, CLLocationManagerDelegate {
 //        print("latitude: \(location.latitude) longitude: \(location.longitude)")
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return placeholderTitles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listingCell") ?? UITableViewCell(style: .default, reuseIdentifier: "listingCell")
+        
+        assert(indexPath.section == 0)
+        cell.textLabel?.text = placeholderTitles[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print(indexPath.row)
+        // empty dblistingVC for now
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(identifier: "matchedEntryVC")
+        guard let matchedEntryVC = vc as? MatchedEntryViewController else {
+            assertionFailure("couldn't find vc")
+            return
+        }
+        //dblistingVC.delegate = self
 
+        present(matchedEntryVC, animated: true, completion: nil)
+    }
 }
