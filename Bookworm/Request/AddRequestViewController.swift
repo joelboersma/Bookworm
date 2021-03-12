9531b81ddba4067075b9e75d9ab790acacc4d5a4
Joel Boersma <44932998+joelboersma@users.noreply.github.com>
2021-03-11 19:06:15 -0800

    AddRequest: initial search while typing

--- Bookworm/Request/AddRequestViewController.swift
+++ Bookworm/Request/AddRequestViewController.swift
@@ -48,6 +48,8 @@ class AddRequestViewController: UIViewController, UISearchBarDelegate, UITableVi
     // https://openlibrary.org/dev/docs/api/search
     
     var books: [Book] = []
+    var latestSearchResponse: [[String : Any]] = [[:]]
+    var currentQuery = ""
     
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var resultsTableView: UITableView!
@@ -69,6 +71,74 @@ class AddRequestViewController: UIViewController, UISearchBarDelegate, UITableVi
         noSearchResultsLabel.text = ""
     }
     
+    func getResults(num: Int, query: String) {
+        OpenLibraryAPI.search(query) { response, error in
+            if query != self.currentQuery { return }
+            if let unwrappedError = error {
+                print("search error")
+                print(unwrappedError)
+                return
+            }
+            guard let unwrappedResponse = response else {
+                print("no response")
+                return
+            }
+            guard let responseWorks = unwrappedResponse["docs"] as? [[String:Any]] else {
+                print("bad docs response")
+                return
+            }
+            
+            if responseWorks.count == 0 {
+                self.noSearchResultsLabel.text = "No Results Found"
+                return
+            }
+            var queryBooks: [Book] = []
+            for work in responseWorks {
+                if query != self.currentQuery { return }
+                else if self.books.count >= num {
+                    print("search done: " + query)
+//                    self.books = queryBooks
+                    self.resultsTableView.reloadData()
+                    break
+                }
+                if let title = work["title"] as? String,
+                   let authors = work["author_name"] as? [String],
+                   let isbns = (work["isbn"] as? [String])?.filter({ $0.count == 13 }) {
+                    // Required: Title, author(s), and ISBN 13
+                    // Optional: publish date, cover
+                    // Publish date should be retrieved when user clicks on UITableView cell
+                    for isbn in isbns {
+                        if query != self.currentQuery { return }
+                        else if self.books.count >= num {
+                            break
+                        }
+                        let book = Book(title: title, isbn: isbn, authors: authors, publishDate: nil)
+                        self.books.append(book)
+                        self.resultsTableView.reloadData()
+                        
+                        //get medium sized cover for the add request listing view
+                        OpenLibraryAPI.cover(key: .ISBN, value: isbn, size: .M) { response, error in
+                            if let unwrappedError = error {
+                                print("error finding cover")
+                                print(unwrappedError)
+                            }
+                            else if let coverResponse = response {
+                                guard let imageData: Data = coverResponse["imageData"] as? Data else {
+                                    print("bad image data")
+                                    return
+                                }
+                                book.coverImageM = imageData
+                            }
+                            self.resultsTableView.reloadData()
+                        }
+                    }
+                }
+            }
+            self.resultsTableView.reloadData()
+            self.start()
+        }
+    }
+    
     func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
         self.tapGestureRecognizer.isEnabled = true
         return true
@@ -79,118 +149,32 @@ class AddRequestViewController: UIViewController, UISearchBarDelegate, UITableVi
         return true
     }
     
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        if let text = searchBar.searchTextField.text {
-            if text.filter({!$0.isWhitespace}).isEmpty {
-                bigSearchLabel.isHidden = false
-                resultsTableView.isHidden = true
-                noSearchResultsLabel.text = ""
-                return
-            }
-        }
-        
-        bigSearchLabel.isHidden = true
-        resultsTableView.isHidden = false
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+        currentQuery = searchText
+        books.removeAll()
+        resultsTableView.reloadData()
         noSearchResultsLabel.text = ""
-            
+        if searchText.filter({!$0.isWhitespace}).isEmpty {
+            bigSearchLabel.isHidden = false
+            resultsTableView.isHidden = true
+        }
+        else {
+            bigSearchLabel.isHidden = true
+            resultsTableView.isHidden = false
+            getResults(num: 25, query: searchText)
+        }
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
         // Hide keyboard
         view.endEditing(true)
-        
-        // Clear books
-        books.removeAll()
-        self.resultsTableView.reloadData()
-        
-        // Search for new books
-        if let searchText = searchBar.text {
-            //display activity indicator
-            self.wait()
-            OpenLibraryAPI.search(searchText) { response, error in
-                if let unwrappedError = error {
-                    print("search error")
-                    print(unwrappedError)
-                    return
-                }
-                guard let unwrappedResponse = response else {
-                    print("no response")
-                    return
-                }
-                guard let responseWorks = unwrappedResponse["docs"] as? [[String:Any]] else {
-                    print("bad docs response")
-                    return
-                }
-                
-                if responseWorks.count == 0{
-                    self.noSearchResultsLabel.text = "No Results Found"
-                }
-                for work in responseWorks {
-                    if self.books.count >= 25 {
-//                        print("woohoo 25 books")
-                        break
-                    }
-                    if let title = work["title"] as? String,
-                       let authors = work["author_name"] as? [String],
-                       let isbns = (work["isbn"] as? [String])?.filter({ $0.count == 13 }) {
-                        // Required: Title, author(s), and ISBN
-                        // Optional: publish date, cover
-                        // Publish date should be retrieved when user clicks on UITableView cell
-                        for isbn in isbns {
-                            if self.books.count >= 25 {
-                                break
-                            }
-                            let book = Book(title: title, isbn: isbn, authors: authors, publishDate: nil)
-                            
-                            
-                            self.books.append(book)
-//                            OpenLibraryAPI.cover(key: .ISBN, value: isbn, size: .S) { response, error in
-//                                if let unwrappedError = error {
-//                                    print("error finding cover")
-//                                    print(unwrappedError)
-//                                }
-//                                else if let coverResponse = response {
-//                                    let coverS = coverResponse["imageData"] as? Data
-//                                    book.coverImageS = coverS
-//                                    
-//                                }
-//                                self.resultsTableView.reloadData()
-//
-//                            }
-                            
-                            //get medium sized cover for the add request listing view
-                            OpenLibraryAPI.cover(key: .ISBN, value: isbn, size: .M) { response, error in
-                                if let unwrappedError = error {
-//                                    print("error finding cover")
-//                                    print(unwrappedError)
-                                }
-                                else if let coverResponse = response {
-                                    guard let imageData: Data = coverResponse["imageData"] as? Data else {
-                                        print("bad image data")
-                                        return
-                                    }
-//                                    let coverM = coverResponse["imageData"] as? Data
-//                                    book.coverImageM = coverM
-                                    book.coverImageM = imageData
-                                }
-                                self.resultsTableView.reloadData()
-
-                            }
-// NOTE: not sure if it's necessary to get large image
-//                            OpenLibraryAPI.cover(key: .ISBN, value: isbn, size: .L) { response, error in
-//                                if let unwrappedError = error {
-//                                    print("error finding cover")
-//                                    print(unwrappedError)
-//                                }
-//                                else if let coverResponse = response {
-//                                    let coverL = coverResponse["imageData"] as? Data
-//                                    book.coverImageL = coverL
-//                                }
-//                            }
-                        }
-                    }
-                }
-                self.resultsTableView.reloadData()
-                self.start()
-            }
-        }
+    }
+    
+    // table view scrolling
+    func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        // Hide keyboard
+        view.endEditing(true)
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
