7bd389a19e968f79eacbea48d7b304ffdc707e67
Mohammed Haque <msyamhaque@gmail.com>
2021-03-16 02:52:24 -0700

    implementation with mapkit driving distance

--- Bookworm/Home/HomeViewController.swift
+++ Bookworm/Home/HomeViewController.swift
@@ -67,7 +67,7 @@ class HomeViewController: UIViewController, UISearchBarDelegate, UITableViewDele
     var currentQuery = ""
     let locationManager = CLLocationManager()
     var locationUpdateTimer = Timer()
-    var currLocation: CLLocation = CLLocation(latitude: 0.0, longitude: 0.0)
+    var currLocation: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)
     var ref = Database.database().reference()
     var storageRef = Storage.storage().reference()
     
@@ -433,19 +433,40 @@ class HomeViewController: UIViewController, UISearchBarDelegate, UITableViewDele
                 return
             }
             if let placemark = placemarks?.first {
-                guard let cellLocation: CLLocation = placemark.location else { return }
-                let distanceInMeters = Measurement(value: cellLocation.distance(from: self.currLocation), unit: UnitLength.meters)
-                let distanceInMiles = distanceInMeters.converted(to: UnitLength.miles)
-                let distanceString = String(format: "%.1f", distanceInMiles.value) + " miles"
-                completion(distanceString)
+                guard let cellLocation: CLLocationCoordinate2D = placemark.location?.coordinate else { return }
+//                let distanceInMeters = Measurement(value: cellLocation.distance(from: self.currLocation), unit: UnitLength.meters)
+//                let distanceInMiles = distanceInMeters.converted(to: UnitLength.miles)
+//                let distanceString = String(format: "%.1f", distanceInMiles.value) + " miles"
+                    let request = MKDirections.Request()
+                // source and destination are the relevant MKMapItems
+                    let source = MKPlacemark(coordinate: self.currLocation)
+                    let destination = MKPlacemark(coordinate: cellLocation)
+                    request.source = MKMapItem(placemark: source)
+                    request.destination = MKMapItem(placemark: destination)
+
+                    // Specify the transportation type
+                    request.transportType = MKDirectionsTransportType.automobile;
+
+                    // If open to getting more than one route,
+                    // requestsAlternateRoutes = true; else requestsAlternateRoutes = false;
+                    request.requestsAlternateRoutes = true
+
+                    let directions = MKDirections(request: request)
+
+                    directions.calculate { (response, error) in
+                        if let response = response, let route = response.routes.first {
+                            completion(MKDistanceFormatter().string(fromDistance: route.distance))
+                        }
+                    }
+//                completion(distanceString)
             }
         }
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        guard let location: CLLocation = manager.location else { return }
+        guard let location: CLLocationCoordinate2D = manager.location?.coordinate else { return }
         self.currLocation = location
-        print("Location updated:", location)
+//        print("Location updated:", location)
 //        self.books.removeAll()
 //        self.ref.child("Posts").removeAllObservers()
 //        self.makeDatabaseCallsforReload(filterOption: self.filterValue, distanceFilterOption: self.distanceFilter, searchQuery: currentQuery)
